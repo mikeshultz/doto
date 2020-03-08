e716a139dda5af33617c7fe2914bbd29a2e43e66
Mike Shultz <shultzm@gmail.com>
2020-03-08 00:22:51 -0700

    make today and tomorrow events distinct

--- doto-ui/src/const.js
+++ doto-ui/src/const.js
@@ -1,1 +1,2 @@
-export const  CALENDAR_REFETCH_INTERVAL = 900000 // 15m
+export const CALENDAR_REFETCH_INTERVAL = 900000 // 15m
+export const ONE_DAY = 86400000 // in ms
--- doto-ui/src/pages/Events.js
+++ doto-ui/src/pages/Events.js
@@ -2,11 +2,38 @@ import React, { useEffect, useRef } from 'react'
 import { useQuery } from '@apollo/react-hooks'
 
 import { GET_EVENTS } from '../queries'
-import { CALENDAR_REFETCH_INTERVAL } from '../const'
+import { CALENDAR_REFETCH_INTERVAL, ONE_DAY } from '../const'
 import Event from '../components/Event'
 import AddCalendarModal from '../components/AddCalendarModal'
 
-//import './Events.css'
+import './Events.css'
+
+function toStringDate(v) {
+  let day
+  if (v instanceof Date) {
+    day = v
+  } else if (typeof v === 'number') {
+    day = new Date(v)
+  } else {
+    day = new Date(v.date ? v.date : v.datetime)
+  }
+  const y = day.getFullYear()
+  const m = String(day.getMonth()).padStart(2, '0')
+  const d = String(day.getDate()).padStart(2, '0')
+  return `${y}${m}${d}`
+}
+
+function dgt(a, b) {
+  return toStringDate(a) > toStringDate(b)
+}
+
+function dgte(a, b) {
+  return toStringDate(a) >= toStringDate(b)
+}
+
+function dlte(a, b) {
+  return toStringDate(a) <= toStringDate(b)
+}
 
 function Events(props) {
   const { modalState, setModalState } = props
@@ -53,16 +80,43 @@ function Events(props) {
     </div>
   }
 
-  const events = data.events.map(ev => {
-    console.log('rendering events: ', ev.id)
+  const now = new Date()
+  const plus24 = +new Date() + ONE_DAY
+
+  const isToday = (v) => dlte(v.start, now) && dgte(v.end, now)
+  const isTomorrow = (v) => dlte(v.start, plus24) && dgte(v.end, plus24)
+  const isFuture = (v) => dgt(v.end, plus24)
+
+  const today = data.events.filter(ev => isToday(ev))
+  const tomorrow = data.events.filter(ev => isTomorrow(ev))
+  const future = data.events.filter(ev => isFuture(ev))
+
+  const todaysEvents = today.map(ev => {
+    console.log('today ev: ', ev.id)
+    return <Event key={ev.id} event={ev} />
+  })
+
+  const tomorrowsEvents = tomorrow.map(ev => {
+    console.log('tomorrow ev: ', ev.id)
+    return <Event key={ev.id} event={ev} />
+  })
+
+  const laterEvents = future.map(ev => {
+    console.log('not today ev: ', ev.id)
     return <Event key={ev.id} event={ev} />
   })
 
   return (
     <div id="events">
       {authNotices}
-      <ul className="events-container">
-        {events}
+      <ul className="events-container today">
+        {todaysEvents}
+      </ul>
+      <ul className="events-container tomorrow">
+        {tomorrowsEvents}
+      </ul>
+      <ul className="events-container future">
+        {laterEvents}
       </ul>
       <AddCalendarModal modalState={modalState} setModalState={setModalState} />
     </div>
